1cf1be0783309de3d2c7830bb68595a4331525ed
Adolfo Jara <gavilanesadolfo@gmail.com>
2020-05-13 12:16:03 -0500

    Creacion de los metodos en la vista del usuario y del telefono

--- src/es/ups/edu/vista/VistaTelefono.java
+++ src/es/ups/edu/vista/VistaTelefono.java
@@ -5,10 +5,110 @@
  */
 package es.ups.edu.vista;
 
+import es.ups.edu.modelo.Telefono;
+import es.ups.edu.modelo.Usuario;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Adolfo
  */
 public class VistaTelefono {
-    
+
+    private Scanner entrada;
+
+    public VistaTelefono() {
+       entrada = new Scanner(System.in);
+    }
+
+   public int menuEditarTelefonoUsuario() {
+        entrada = new Scanner(System.in);
+        System.out.println("\nDigite la opcion que desea realizar:\n"
+                + "1.-Ingresar un nuevo numero" + "\n2.-Editar un telefono"
+                + "\n3.-Eliminar un telefono" + "\n4.-Cerrar sesion y volver al "
+                + "menu");
+        int opcionSesion = entrada.nextInt();
+
+        return opcionSesion;
+    }
+
+    public Telefono ingresarTelefono() {
+        entrada = new Scanner(System.in);
+       System.out.println("Ingresa los datos del Telefono:");
+
+        System.out.println("Ingrese el numero: ");
+       String numero = entrada.next();
+        System.out.println("Ingrese el tipo de telefono: ");
+        String tipo = entrada.next();
+       System.out.println("Ingrese la operadora: ");
+        String operadora = entrada.next();
+        System.out.println("Ingrese el codigo: ");
+        int codigo = entrada.nextInt();
+        return new Telefono(codigo, numero, tipo, operadora);
+    }
+
+    public int confirmarCodigo() {
+        entrada = new Scanner(System.in);
+        System.out.println("\nConfirme el codigo del telefono: ");
+        int codigo = entrada.nextInt();
+        return codigo;
+    }
+
+    public int pedirCodigo() {
+
+        entrada = new Scanner(System.in);
+        System.out.println("Ingresa el codigo del telefono a actualizar");
+       int id = entrada.nextInt();
+
+        return id;
+    }
+
+    public Telefono actualizarTelefono() {
+        entrada = new Scanner(System.in);
+       System.out.println("\nIngrese los nuevos datos: ");
+        System.out.println("\nIngresa el mismo codigo: ");
+        int id = entrada.nextInt();
+        System.out.println("Ingrese el numero: ");
+       String numero = entrada.next();
+        System.out.println("Ingrese el tipo: ");
+        String tipo = entrada.next();
+        System.out.println("Ingrese la operadora: ");
+       String operadora = entrada.next();
+
+        return new Telefono(id, numero, tipo, operadora);
+    }
+    public int eliminarTelefono() {
+        entrada = new Scanner(System.in);
+        System.out.println("Ingresa el codigo del telefono a eliminar");
+        int id = entrada.nextInt();
+        return id;
+    }
+
+    public int buscarTelefono() {
+        entrada = new Scanner(System.in);
+        System.out.println("Ingresa el id de la direccion a buscar");
+        int id = entrada.nextInt();
+       return id;
+    }
+
+    public void imprimirTelefonosUsuario(Usuario usuario) {
+        if (usuario.getListaTelefonos().isEmpty()) {
+            System.out.println("El usuario no tiene telefonos agregados");
+        } else {
+           for (Telefono telefono : usuario.getListaTelefonos()) {
+                System.out.println(telefono.toString());
+           }
+        }
+    }
+    public void verDireccion(Telefono telefono) {
+        System.out.println("Datos del Cliente: " + telefono);
+    }
+
+    public void verTelefonos(List<Telefono> telefonos) {
+        for (Telefono direccion : telefonos) {
+            System.out.println("Datos del telefono: " + direccion);
+        }
+    }
+
 }
--- src/es/ups/edu/vista/VistaUsuario.java
+++ src/es/ups/edu/vista/VistaUsuario.java
@@ -5,10 +5,122 @@
  */
 package es.ups.edu.vista;
 
+import es.ups.edu.modelo.Usuario;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author Adolfo
  */
 public class VistaUsuario {
+   
+     private Scanner entrada;
+
+    public VistaUsuario() {
+        entrada = new Scanner(System.in);
+    }
+    
+    public int menu() {
+        entrada = new Scanner(System.in);
+       System.out.println("\nDigite el numero de accion que desea "
+                + "ejecutar:\n" + "1.-Crear un nuevo usuario"
+                + "\n2.-Iniciar sesion" + "\n3.-Imprimir telefonos de un usuario "
+                + "\n4.-Buscar un usuario" + "\n5.-Imprimir todos los usuarios" 
+                + "\n6.-Imprimir todos los telefonos registrados" + "\n7.-Salir");
+        int opcionMenu = entrada.nextInt();
+
+       return opcionMenu;
+    }
+    
+    
+    public Usuario ingresarCliente() {
+       entrada = new Scanner(System.in);
+        System.out.println("Ingresa los datos del cliente");
+       System.out.println("Ingrese la cedula: ");
+        String cedula = entrada.next();
+       System.out.println("Ingrese el nombre: ");
+        String nombre = entrada.next();
+        System.out.println("Ingrese el apellido: ");
+        String apellido = entrada.next();
+        System.out.println("Ingrese el correo: ");
+        String correo = entrada.next();
+        System.out.println("Ingrese la contraseÃ±a: ");
+        String contraseña = entrada.next();
+        
+        return new Usuario(cedula, nombre, apellido, correo, contraseña);
+    }
+
+    public String iniciarSesionCorreo() {
+        entrada = new Scanner(System.in);
+
+       System.out.println("Escriba su correo: ");
+        String correo = entrada.next();
+
+        return correo;
+
+    }
+    
+    public String iniciarSesionContraseña() {
+       entrada = new Scanner(System.in);
+
+        System.out.println("Escriba su contraseÃ±a: ");
+        String contraseña = entrada.next();
+
+        return contraseña;
+    }
+   
+    
+    
+    
+    public Usuario actualizarCliente() {
+        entrada = new Scanner(System.in);
+       System.out.println("Ingresa el id del cliente a actualizar");
+        String id = entrada.next();
+       System.out.println("Ingrese los nuevos Datos (nombre, apellido)");
+        String nombre = entrada.next();
+        String apellido = entrada.next();
+        String correo = entrada.next();
+        String contraseña = entrada.next();
+        return new Usuario(id, nombre, apellido, correo, contraseña);
+    }
+
+    public Usuario eliminarCliente() {
+        entrada = new Scanner(System.in);
+        System.out.println("Ingresa el id del cliente a eliminar");
+       String id = entrada.next();
+        return new Usuario(id, null, null, null, null);
+    }
+
+    public String confirmacionCedula(){
+        entrada = new Scanner(System.in);
+       System.out.println("\nEscriba su cedula para confirmar el cambio:");
+        String cedula = entrada.next();
+        return cedula;
+   }
+    
+    public String buscarCliente() {
+        entrada = new Scanner(System.in);
+        System.out.println("Ingresa el correo o cedula del cliente: ");
+        String id = entrada.next();
+        return id;
+    }
+
+   public void verCliente(Usuario cliente) {
+        System.out.println("Datos del Cliente: " + cliente);
+    }
+
+    public void verClientes(Map<String,Usuario> clientes) {
+        for (Map.Entry<String, Usuario> usuario2 : clientes.entrySet()) {
+            String key = usuario2.getKey();
+            Usuario persona = usuario2.getValue();
+            System.out.println("\nClave del usuario: " + key + "\n" + persona);
+       }
+    }
+
+    public void frase(String frase) {
+        System.out.println(frase);
+    }
     
 }
