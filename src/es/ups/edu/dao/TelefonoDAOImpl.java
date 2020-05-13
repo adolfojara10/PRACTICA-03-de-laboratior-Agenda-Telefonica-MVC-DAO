63061b82e9874a6f4bda9667900fe7d16e8ff7a3
Adolfo Jara <gavilanesadolfo@gmail.com>
2020-05-13 12:15:08 -0500

    Implementacion de los metodos declarados en Interface a las clases UsuarioDAOImpl y TelefonoDAOimpl

--- src/es/ups/edu/dao/TelefonoDAOImpl.java
+++ src/es/ups/edu/dao/TelefonoDAOImpl.java
@@ -5,10 +5,65 @@
  */
 package es.ups.edu.dao;
 
+import ec.ups.edu.idao.ITelefonoDAO;
+import es.ups.edu.modelo.Telefono;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author Adolfo
  */
-public class TelefonoDAOImpl {
+public class TelefonoDAOImpl implements ITelefonoDAO{
     
+    private List<Telefono> listaTelefonos;
+
+    public TelefonoDAOImpl() {
+        listaTelefonos = new ArrayList<>();
+   }
+
+    @Override
+    public void create(Telefono telefono) {
+        listaTelefonos.add(telefono);
+   }
+
+    @Override
+    public Telefono read(int id) {
+        for (Telefono telefono : listaTelefonos) {
+            if (telefono.getCodigo() == id) {
+                return telefono;
+            }
+       }
+        return null;
+    }
+
+    @Override
+    public void update(Telefono telefono) {
+       for (int i = 0; i < listaTelefonos.size(); i++) {
+            Telefono c = listaTelefonos.get(i);
+            if (c.getCodigo() == telefono.getCodigo()) {
+                listaTelefonos.set(i, telefono);
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(Telefono telefono) {
+       Iterator<Telefono> it = listaTelefonos.iterator();
+        while (it.hasNext()) {
+            Telefono d = it.next();
+            if (d.getCodigo() == telefono.getCodigo()) {
+                it.remove();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public List<Telefono> findAll() {
+        return listaTelefonos;
+    }
 }
--- src/es/ups/edu/dao/UsuarioDAOImpl.java
+++ src/es/ups/edu/dao/UsuarioDAOImpl.java
@@ -5,10 +5,96 @@
  */
 package es.ups.edu.dao;
 
+import ec.ups.edu.idao.IUsuarioDAO;
+import es.ups.edu.modelo.Usuario;
+import java.util.ArrayList;
+import java.util.*;
+
 /**
  *
  * @author Adolfo
  */
-public class UsuarioDAOImpl {
+public class UsuarioDAOImpl implements IUsuarioDAO{
+   
+    private Map<String, Usuario> listaUsuarios;
+
+    public UsuarioDAOImpl() {
+        listaUsuarios = new HashMap<>();
+   }
+
+    @Override
+    public void create(Usuario cliente) {
+        listaUsuarios.put(cliente.getCedula(),cliente);
+    }
+
+    @Override
+   public Usuario read(String cedula) {
+        //bucle for each para buscar el usuario
+        for (Map.Entry<String, Usuario> usuario : listaUsuarios.entrySet()) {
+            String key = usuario.getKey();
+            Usuario valor = usuario.getValue();
+            //se compara la cedula o el correo
+            if (valor.getCedula().equals(cedula)
+                    || (valor.getCorreo().equals(cedula))) {
+               //se obtiene el usuario con el key
+                Usuario usuario2 = listaUsuarios.get(key);
+                return usuario2;
+            }
+
+       }
+        return null;
+    }
+
+   @Override
+    public void update(Usuario cliente) {
+        for (Map.Entry<String, Usuario> usuario2 : listaUsuarios.entrySet()){
+            Usuario usuario3 = usuario2.getValue();
+            if (usuario3.getCedula().equals(cliente.getCedula())){
+                listaUsuarios.replace(cliente.getCedula(), cliente);
+                break;
+            }
+            
+        }
+
+    }
+
+    @Override
+    public void delete(Usuario cliente) {
+        for (Map.Entry<String, Usuario> usuario2 : listaUsuarios.entrySet()){
+            Usuario usuario3 = usuario2.getValue();
+            if (usuario3.getCedula().equals(cliente.getCedula())){
+                listaUsuarios.remove(cliente.getCedula());
+                break;
+            }
+            
+        }
+    }
+
+       
+   @Override
+    public Usuario iniciarSesion(String correo, String contraseña) {
+        //bucle for each para recorrer los usuarios
+        for (Map.Entry<String, Usuario> usuario : listaUsuarios.entrySet()) {
+            String key = usuario.getKey();
+           Usuario valor = usuario.getValue();
+            //compara primero el correo
+            if (valor.getCorreo().equals(correo)) {
+                //si el correo es igual, compara la contraseÃ±a
+                if (valor.getContraseña().equals(contraseña)) {
+                    Usuario usu = new Usuario();
+                    usu = listaUsuarios.get(key);
+                    return usu;
+                }
+            }
+
+        }
+
+        return null;
+    }
     
+    @Override
+    public Map<String, Usuario> findAll() {
+        return listaUsuarios;
+    }
+
 }
